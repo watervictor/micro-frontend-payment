885da1d0520d7e2e79bc8369a386842a8f8b93fe
victor-okereke <okerekev324@gmail.com>
2023-06-13 06:04:25 -0700

    finished upper part UI

--- src/blocks/components/payment.tsx
+++ src/blocks/components/payment.tsx
@@ -1,27 +1,85 @@
+import React from "react";
+import visa from "../../assets/visa.webp";
+import master from "../../assets/master.webp";
+import disc from "../../assets/disc.webp";
+import paypal from "../../assets/paypal.webp";
+
 const Payment = () => {
+  const [first, setFirst] = React.useState<boolean>(true);
+
+  const toSecond = () => {
+    setFirst(false);
+  };
+
   return (
-    <div className="w-full h-[fit-content] lg:flex-1 bg-[#ffecf1] p-[15px] text-xl font-[500] flex flex-col gap-3">
-      <div>Cart totals</div>
-      <div className="w-full flex justify-between">
-        <div>$49.65</div>
-        <div className="text-[#b9b9b9] font-[300] flex">
-          <div className="line-through">$99.99 </div>
-          <div> 50% off</div>
+    <div className="w-full h-[fit-content] md:w-[320px] bg-[#ffecf1] p-[15px] text-xl font-[500] flex flex-col gap-3">
+      {first ? (
+        <>
+          <div>Cart totals</div>
+          <div className="w-full flex justify-between">
+            <div>$49.65</div>
+            <div className="text-[#b9b9b9] font-[300] flex">
+              <div className="line-through">$99.99 </div>
+              <div> 50% off</div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <div>Cart Details</div>
+          <div className="flex w-full justify-between">
+            <img src={visa} />
+            <img src={master} />
+            <img src={disc} />
+            <img src={paypal} />
+          </div>
+          <div className="flex flex-col gap-4">
+            <input
+              className="w-full h-10 p-4 outline-none border-slate-300 border-[1px] font-[400] rounded-[3px] text-sm text-slate-500"
+              type="text"
+              placeholder="Name on card"
+            />
+            <input
+              className="w-full h-10 p-4 outline-none border-slate-300 border-[1px] font-[400] rounded-[3px] text-sm text-slate-500"
+              type="text"
+              placeholder="Card number"
+            />
+            <input
+              className="w-full h-10 p-4 outline-none border-slate-300 border-[1px] font-[400] rounded-[3px] text-sm text-slate-500"
+              type="text"
+              placeholder="Expiration date"
+            />
+            <input
+              className="w-full h-10 p-4 outline-none border-slate-300 border-[1px] font-[400] rounded-[3px] text-sm text-slate-500"
+              type="text"
+              placeholder="Security code"
+            />
+          </div>
+          <div className="w-full flex justify-between">
+            <div>Total:</div>
+            <div>$49.65</div>
+          </div>
         </div>
-      </div>
-      <div className="w-full cursor-pointer text-lg font-normal h-12 flex justify-center items-center text-white bg-[#e96269] rounded">
+      )}
+      <div
+        onClick={toSecond}
+        className="w-full transition-all hover:shadow-red-300 hover:shadow-xl cursor-pointer text-lg font-normal h-12 flex justify-center items-center text-white bg-[#e96269] rounded">
         Check Out
       </div>
-      <div className="w-full h-12 flex items-center mt-1">
-        <input
-          className="h-full text-sm font-light w-[80%] rounded-tl rounded-bl focus:border-[0px] focus:outline-[0px] border-[1px] p-3 border-[#e96269]"
-          type="text"
-          placeholder="Enter Coupon"
-        />
-        <div className="flex-1 h-full cursor-pointer text-lg font-normal flex justify-center items-center text-white bg-[#e96269] rounded-tr rounded-br">
-          Apply
-        </div>
-      </div>
+      {first ? (
+        <>
+          <div className="w-full h-12 flex items-center mt-1">
+            <input
+              className="h-full text-sm font-light w-[70%] rounded-tl rounded-bl focus:border-[0px] focus:outline-[0px] border-[1px] p-3 border-[#e96269]"
+              type="text"
+              placeholder="Enter Coupon"
+            />
+            <div className="flex-1 transition-all hover:shadow-red-300 hover:shadow-xl h-full cursor-pointer text-lg font-normal flex justify-center items-center text-white bg-[#e96269] rounded-tr rounded-br">
+              Apply
+            </div>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
--- src/blocks/courses.tsx
+++ src/blocks/courses.tsx
@@ -1,14 +1,26 @@
 import { Course, Payment } from ".";
 import dummy from "../assets/cs.webp";
-import dummy1 from "../assets/sd.webp";
+import dummy1 from "../assets/ds.webp";
+import dummy2 from "../assets/sd.webp";
+import dummy3 from "../assets/pr1.webp";
+import dummy4 from "../assets/pr2.webp";
+import dummy5 from "../assets/pr3.webp";
 
 const Courses = () => {
   return (
     <div className="w-11/12 flex flex-col items-center">
-      <div className="w-full flex flex-col lg:flex-row gap-8 mt-[-47px] md:mt-[-53px]">
-        <div className="w-full lg:w-[65%] flex flex-col gap-8">
+      <div className="w-full flex flex-col md:flex-row gap-8 mt-[-47px] md:mt-[-53px]">
+        <div className="w-full md:w-[67%] xl:w-[71%] flex flex-col gap-8">
           <Course
-            authorCover={dummy}
+            authorCover={dummy3}
+            titleCover={dummy}
+            title="statistics data science and business analysis"
+            author="paul molive"
+            present="100.00"
+            former="150.00"
+          />
+          <Course
+            authorCover={dummy4}
             titleCover={dummy1}
             title="statistics data science and business analysis"
             author="paul molive"
@@ -16,8 +28,8 @@ const Courses = () => {
             former="150.00"
           />
           <Course
-            authorCover={dummy}
-            titleCover={dummy1}
+            authorCover={dummy5}
+            titleCover={dummy2}
             title="statistics data science and business analysis"
             author="paul molive"
             present="100.00"
