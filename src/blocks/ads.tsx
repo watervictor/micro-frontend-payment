5f465d47bc02b18aac05b0e8cd738706ebd39405
Dc Valerian <Dc-Valerian>
2023-06-13 15:06:04 +0100

    added by valerian

--- src/blocks/ads.tsx
+++ src/blocks/ads.tsx
@@ -1,5 +1,38 @@
-const Ads = () => {
-  return <div>Ads</div>;
-};
+import styled from "styled-components"
 
-export default Ads;
+const ads = () => {
+  return (
+    <div>
+    <Container>
+        <Wrapper>
+           <Title>
+              Top Education offers and deals are listed here
+           </Title>
+        </Wrapper>
+    </Container>
+    </div>
+  )
+}
+
+export default ads
+
+const Title = styled.div`
+font-weight: 500;
+display: flex;
+align-items: center;
+margin-top: 40px;
+`
+const Wrapper = styled.div`
+width: 90%;
+height: 90%;
+background-color: yellow;
+`
+
+const Container = styled.div`
+background-color: red;
+height: 500px;
+width: 100vw;
+display: flex;
+align-items: center;
+justify-content: center;
+`
